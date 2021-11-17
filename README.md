44f1b33ee07abdf4d350d2436c6b690737ba6a80
Tejash Mehta <freakytejash@gmail.com>
2021-11-17 16:57:38 +0100

    Readme and UI changes

---
+++ README.md
@@ -0,0 +1,24 @@
+# Run Application
+Step-1: Open "Android studio" then choose "Check out project from Version control"
+
+Step-2: From the options choose "Git", then provide the following url "https://github.com/2021-DEV-068/TicTacToe.git" and tap on "Clone"
+
+Step-3: Once the checkout completes it should open up the project.
+
+Step - 4: Click "Run App" from the toolbar to run the application on the Physical device or Emulator.
+
+step - 5: Use touch to place "X"'s and "O"'s into the game board, Player 1 always goes "X" as first and Player 2 will have "O" to play the game. Result will be announced based on the above game rules and game will be restarted.
+
+# Run Test Cases
+Step-1: Click on "Run" option in toolbar and edit the run configure to point testcases within the application
+
+Step-2: Please find the test class files and its path in below table:
+
+| File Name  | File Path|
+| ------------- | ------------- |
+| BoardTest  | "app/src/test/java/com.demo.tictactoe.model.BoardTest"  |
+| CellTest  | "app/src/test/java/com.demo.tictactoe.model.CellTest"  |
+| PlayerTest  | "app/src/test/java/com.demo.tictactoe.model.PlayerTest"  |
+| ViewModelTest  | "app/src/test/java/com.demo.tictactoe.viewmodel.ViewModelTest"  |
+
+Step-3: You can choose to test all the cases by running the whole file or choose any particular test cases by opening it
--- app/src/main/java/com/demo/tictactoe/TicTacToeActivity.kt
+++ app/src/main/java/com/demo/tictactoe/TicTacToeActivity.kt
@@ -2,11 +2,50 @@ package com.demo.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.demo.tictactoe.databinding.ActivityMainBinding
+import com.demo.tictactoe.viewmodel.GameViewModel
 
 class TicTacToeActivity : AppCompatActivity() {
+    private lateinit var activityGameBinding: ActivityMainBinding
+    private val gameViewModel: GameViewModel by lazy { ViewModelProvider(this).get(GameViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        gameViewModel.init(getString(R.string.player_one), getString(R.string.player_two))
+        activityGameBinding.gameViewModel = gameViewModel
+        invalidateGameBoard()
+    }
+
+    private fun invalidateGameBoard() {
+        activityGameBinding.gameViewModel = gameViewModel
+        setUpOnGameEndListener()
+    }
+
+    private fun setUpOnGameEndListener() {
+        gameViewModel.getWinner().observe(this,  { playerName ->
+            showGameResult("${getString(R.string.winner_is)} $playerName")
+        })
+        gameViewModel.getNoWinner().observe(this, { message ->
+            showGameResult(message)
+        })
+    }
+
+    private fun resetGame() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            gameViewModel.resetGame()
+            invalidateGameBoard()
+        }, 500)
+    }
+
+    private fun showGameResult(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        resetGame()
     }
 }
