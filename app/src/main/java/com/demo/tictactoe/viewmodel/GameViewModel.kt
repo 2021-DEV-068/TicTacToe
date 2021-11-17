ea46ac7cf77794bca6fd8af35a6426331801ec64
Tejash Mehta <freakytejash@gmail.com>
2021-11-17 14:30:00 +0100

    Added view model test and view model and minor UI changes

---
+++ app/src/main/java/com/demo/tictactoe/viewmodel/GameViewModel.kt
@@ -0,0 +1,59 @@
+package com.demo.tictactoe.viewmodel
+
+import androidx.databinding.ObservableArrayMap
+import androidx.lifecycle.ViewModel
+import com.demo.tictactoe.model.Cell
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.demo.tictactoe.model.Board
+
+class GameViewModel: ViewModel() {
+
+    lateinit var board: Board
+    lateinit var cells: ObservableArrayMap<String, String>
+    private lateinit var playerOne: String
+    private lateinit var playerTwo: String
+    private var winner = MutableLiveData<String>()
+    private var noWinner = MutableLiveData<String>()
+
+    fun init(playerOne: String, playerTwo: String){
+        this.playerOne = playerOne
+        this.playerTwo = playerTwo
+        board = Board(playerOne, playerTwo)
+        cells = ObservableArrayMap()
+    }
+
+    fun getWinner(): LiveData<String> = winner
+    fun getNoWinner(): LiveData<String> = noWinner
+
+    fun onClickedCellAt(row: Int, column: Int) {
+        when { isCellEmpty(row, column) -> updatePlayerValueInSelectedCell(row, column) }
+    }
+
+    private fun updatePlayerValueInSelectedCell(row: Int, column: Int) {
+        board.cells[row][column] = notifyCurrentPlayer()
+        cells[stringFromNumbers(row, column)] = getCurrentPlayerValue()
+        updateGameStatus()
+    }
+
+    fun updateGameStatus() = when {
+            board.isWinnerAvailable() -> winner.postValue(board.currentPlayer.name)
+            board.isFull() -> noWinner.postValue(Board.NO_WINNER_FOUND)
+            else -> board.switchPlayer()
+    }
+
+    fun stringFromNumbers(vararg numbers: Int): String {
+        val sNumbers = StringBuilder()
+        numbers.forEach { number -> sNumbers.append(number) }
+        return sNumbers.toString()
+    }
+
+    fun resetGame(){
+        init(playerOne, playerTwo)
+        winner = MutableLiveData()
+        noWinner = MutableLiveData()
+    }
+    private fun isCellEmpty(row: Int, column: Int) = board.cells[row][column].isEmptyCell
+    private fun notifyCurrentPlayer() = Cell(board.currentPlayer)
+    private fun getCurrentPlayerValue() = board.currentPlayer.value
+}
---
+++ app/src/test/java/com/demo/tictactoe/viewmodel/ViewModelTest.kt
@@ -0,0 +1,217 @@
+package com.demo.tictactoe.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.demo.tictactoe.model.Board
+import com.demo.tictactoe.model.Cell
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+class ViewModelTest {
+
+    private val viewModel = GameViewModel()
+    private val playerOne = "Tejash"
+    private val playerTwo = "Ajay"
+    private val playerOneValue = Board.PLAYER_ONE_VALUE
+    private val ROW_INDEX = 0
+    private val COLUMN_INDEX = 0
+
+    @Rule
+    @JvmField
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `Given function should return player one name`(){
+        val expectedResult = playerOne
+
+        viewModel.init(playerOne, playerTwo)
+
+        val actualResult = viewModel.board.player1.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return expected result`(){
+
+        viewModel.init(playerOne, playerTwo)
+
+        val actualResult = viewModel.cells
+
+        Assert.assertNotNull(actualResult)
+    }
+
+    @Test
+    fun `Given function should return expected result, when 0,0 column clicked in the game`(){
+
+        viewModel.init(playerOne, playerTwo)
+        viewModel.onClickedCellAt(ROW_INDEX,COLUMN_INDEX)
+
+        val actualResult =  viewModel.board.cells[ROW_INDEX][COLUMN_INDEX].player?.name
+
+        Assert.assertEquals(playerOne, actualResult)
+    }
+
+    @Test
+    fun `Given function should return String of given numbers`(){
+
+        val actualResult = viewModel.stringFromNumbers(0,1)
+
+        Assert.assertEquals("01", actualResult)
+    }
+
+    @Test
+    fun `Given function should return player one value, when 0,0 column clicked in the game`(){
+
+        viewModel.init(playerOne, playerTwo)
+        viewModel.onClickedCellAt(ROW_INDEX,COLUMN_INDEX)
+
+        val actualResult =  viewModel.cells[viewModel.stringFromNumbers(ROW_INDEX, COLUMN_INDEX)]
+
+        Assert.assertEquals(playerOneValue, actualResult)
+    }
+
+    @Test
+    fun `Given function should not return null`(){
+
+        val actualResult =  viewModel.getWinner()
+
+        Assert.assertNotNull(actualResult)
+    }
+
+    @Test
+    fun `Given function should return winner in Live Data, if game has winner in the board`(){
+        val expectedResult = playerOne
+
+        viewModel.init(playerOne, playerTwo)
+        val cell = Cell(viewModel.board.player1)
+        viewModel.board.cells[0][0] = cell
+        viewModel.board.cells[0][1] = cell
+        viewModel.board.cells[0][2] = cell
+        viewModel.updateGameStatus()
+
+        val actualResult =  viewModel.getWinner().value
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return not null as expected`(){
+
+        val actualResult =  viewModel.getNoWinner()
+
+        Assert.assertNotNull(actualResult)
+    }
+
+    @Test
+    fun `Given function should return no winner in Live Data, if game has no winner in the board`(){
+        val expectedResult = "No winner found!"
+
+        viewModel.init(playerOne, playerTwo)
+        val cell1 = Cell(viewModel.board.player1)
+        val cell2 = Cell(viewModel.board.player2)
+
+        viewModel.board.cells[0][0] = cell1
+        viewModel.board.cells[0][1] = cell1
+        viewModel.board.cells[0][2] = cell2
+
+        viewModel.board.cells[1][0] = cell2
+        viewModel.board.cells[1][1] = cell2
+        viewModel.board.cells[1][2] = cell1
+
+        viewModel.board.cells[2][0] = cell1
+        viewModel.board.cells[2][1] = cell1
+        viewModel.board.cells[2][2] = cell2
+        viewModel.updateGameStatus()
+        val actualResult =  viewModel.getNoWinner().value
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should switch the player, when game board is not full and no winner found`(){
+        val expectedResult = playerTwo
+
+        viewModel.init(playerOne, playerTwo)
+        val cell = Cell(viewModel.board.player1)
+        viewModel.board.cells[0][0] = cell
+        viewModel.updateGameStatus()
+
+        val actualResult = viewModel.board.currentPlayer.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+
+    @Test
+    fun `Given function should switch the player after player clicked in board, when game board is not full and no winner found`(){
+        val expectedResult = playerTwo
+
+        viewModel.init(playerOne, playerTwo)
+        viewModel.onClickedCellAt(ROW_INDEX,COLUMN_INDEX)
+
+        val actualResult = viewModel.board.currentPlayer.name
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return expected result, when game is restarted`(){
+        val expectedResult = 0
+
+        viewModel.init(playerOne, playerTwo)
+        viewModel.onClickedCellAt(ROW_INDEX,COLUMN_INDEX)
+        viewModel.cells[viewModel.stringFromNumbers(ROW_INDEX, COLUMN_INDEX)]
+
+        viewModel.resetGame()
+
+        val actualResult = viewModel.cells.size
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should reset winner in Live Data, when game is restarted`(){
+        val expectedResult = null
+
+        viewModel.init(playerOne, playerTwo)
+        val cell = Cell(viewModel.board.player1)
+        viewModel.board.cells[0][0] = cell
+        viewModel.board.cells[0][1] = cell
+        viewModel.board.cells[0][2] = cell
+        viewModel.updateGameStatus()
+        viewModel.resetGame()
+
+        val actualResult =  viewModel.getWinner().value
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should reset the game, when game is restarted`(){
+        val expectedResult = null
+
+        viewModel.init(playerOne, playerTwo)
+        val cell1 = Cell(viewModel.board.player1)
+        val cell2 = Cell(viewModel.board.player2)
+
+        viewModel.board.cells[0][0] = cell1
+        viewModel.board.cells[0][1] = cell1
+        viewModel.board.cells[0][2] = cell2
+
+        viewModel.board.cells[1][0] = cell2
+        viewModel.board.cells[1][1] = cell2
+        viewModel.board.cells[1][2] = cell1
+
+        viewModel.board.cells[2][0] = cell1
+        viewModel.board.cells[2][1] = cell1
+        viewModel.board.cells[2][2] = cell2
+        viewModel.updateGameStatus()
+        viewModel.resetGame()
+
+        val actualResult =  viewModel.getNoWinner().value
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+}
